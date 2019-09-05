9675f9552061ab710401f39f0aa31cf4e0de869c
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-09-05 15:00:06 +0200

    data: Add numpy array constructor for data set

--- remsen/data.py
+++ remsen/data.py
@@ -4,6 +4,12 @@ from typing import Dict
 
 import fiona
 
+import numpy as np
+
+import rasterio
+from rasterio.io import MemoryFile
+from rasterio.mask import mask
+
 from shapely.geometry import MultiPolygon, Polygon, shape
 
 
@@ -32,3 +38,49 @@ def fetch_buildings(path: Path) -> MultiPolygon:
         srid = int(src.crs["init"].split(":")[1])
         assert srid == 25832
         return MultiPolygon([fiona_polygon(item) for item in src])
+
+
+def construct_observation(path, cadastre, buildings):
+    """Construct observation for a given cadastre."""
+    intersecting_buildings = cadastre.intersection(buildings)
+
+    with rasterio.open(path) as src:
+        assert str(src.crs["proj"]) == "utm" and int(src.crs["zone"]) == 32
+        cropped_data, affine_transformation = mask(
+            src, shapes=[cadastre], all_touched=True, crop=True
+        )
+
+        metadata = src.meta.copy()
+        metadata.update(
+            {
+                "height": cropped_data.shape[1],
+                "width": cropped_data.shape[2],
+                "transform": affine_transformation,
+                "driver": "GTiff",
+            }
+        )
+
+        cropped_lidar_file = MemoryFile()
+        with rasterio.open(cropped_lidar_file, "w", **metadata) as file:
+            file.write(cropped_data)
+            if intersecting_buildings:
+                building_data, _ = mask(
+                    file,
+                    shapes=[intersecting_buildings],
+                    all_touched=True,
+                    crop=False,
+                )
+                building_data[building_data > src.nodata] = 1
+                building_data[building_data != 1] = 0
+                building_data = building_data.astype("uint8", copy=False)
+            else:
+                building_data = np.zeros(cropped_data.shape, dtype="uint8")
+
+        metadata = metadata.copy()
+        metadata.update({"dtype": "uint8", "nodata": int(2 ** 8 - 1)})
+
+        building_file = MemoryFile()
+        with rasterio.open(building_file, "w", **metadata) as file:
+            file.write(building_data)
+
+        return cropped_lidar_file, building_file
--- remsen/tests/test_data.py
+++ remsen/tests/test_data.py
@@ -3,28 +3,51 @@ from pathlib import Path
 
 import pytest
 
-from remsen.data import fetch_buildings, fetch_cadastre
+from remsen.data import construct_observation, fetch_buildings, fetch_cadastre
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def data_directory():
     """Directory containing datasets."""
     return Path(__file__).parents[2] / "data"
 
 
-def test_fetch_cadastre(data_directory):
-    """You should be able to retrieve cadastre plot shapely polygons."""
+@pytest.fixture(scope="session")
+def cadastre(data_directory):
     cadastre_path = data_directory / "cadastre.gpkg"
-    assert cadastre_path.exists()
-
     cadastre = fetch_cadastre(path=cadastre_path, index=0)
+    return cadastre
+
+
+@pytest.fixture(scope="session")
+def buildings(data_directory):
+    buildings_path = data_directory / "building.gpkg"
+    buildings = fetch_buildings(path=buildings_path)
+    return buildings
+
+
+def test_fetch_cadastre(cadastre):
+    """You should be able to retrieve cadastre plot shapely polygons."""
     assert int(cadastre.area) == 383
 
 
-def test_fetch_buildings(data_directory):
+def test_fetch_buildings(buildings):
     """You should be able to retrieve all buldings as a MultiPolygon."""
-    buildings_path = data_directory / "building.gpkg"
-    assert buildings_path.exists()
-
-    buildings = fetch_buildings(path=buildings_path)
     assert len(buildings) == 70_082
+
+
+def test_construct_observation(data_directory, cadastre, buildings):
+    """You should be able to construct numpy arrays from data."""
+    lidar_path = data_directory / "lidar.vrt"
+    cropped_lidar_file, building_data_file = construct_observation(
+        path=lidar_path, cadastre=cadastre, buildings=buildings
+    )
+    with cropped_lidar_file.open() as lidar_file:
+        assert lidar_file.nodata == pytest.approx(float(-2 ** 128))
+        lidar_data = lidar_file.read(1)
+        assert lidar_data.dtype == "float32"
+
+    with building_data_file.open() as building_file:
+        assert building_file.nodata == 255.0
+        building_data = building_file.read(1)
+        assert building_data.dtype == "uint8"
