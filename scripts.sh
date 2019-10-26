1e39631d2f626827b36faf4ad73a6ac133ec4a81
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-10-27 00:59:52 +0200

    docker: Add tensorboard function to scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -2,6 +2,7 @@ function project::run {
   # This function requires you to build the tensorflow image first:
   # $ sudo docker build . -t tensorflow
   sudo docker run \
+    --name project \
     --user $(id -u):$(id -g) \
     --runtime=nvidia \
     --gpus all \
@@ -12,4 +13,21 @@ function project::run {
     -p 6006:6006 \
     --volume $PWD:/code \
     tensorflow $1
+}
+
+
+function project::tensorboard {
+  sudo docker exec \
+    --interactive \
+    --tty \
+    --user $(id -u):$(id -g) \
+    --workdir /code \
+    --detach \
+    project \
+    tensorboard \
+      --port 6006 \
+      --host 0.0.0.0 \
+      --logdir /code/.cache/tensorboard \
+
+  echo 'TensorBoard available at: http://0.0.0.0:6006/'
 }
