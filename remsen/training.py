18d5c58344943119b37087ca2b5c2aa30e69ad2c
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-10-27 00:43:17 +0200

    training: Add Trainer class
    Responsible for training models with proper callbacks

--- remsen/training.py
+++ remsen/training.py
@@ -1,14 +1,22 @@
 from pathlib import Path
 
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.callbacks import (
+    EarlyStopping,
+    ModelCheckpoint,
+    TensorBoard
+)
+from tensorflow.keras.models import Model
+
+from remsen.data import Dataset
 
 
 def get_callbacks(
     model_name: str,
+    cache_path: Path,
     early_stopping: bool = False,
+    verbose: int = 0,
 ):
-    cache_path = Path(".cache/models")
-    model_cache_path = cache_path / model_name
+    model_cache_path = cache_path / "models" / model_name
 
     model_cache_path.mkdir(parents=True, exist_ok=True)
 
@@ -18,15 +26,15 @@ def get_callbacks(
         filepath=str(model_cache_path / "val_loss.ckpt"),
         save_weights_only=True,
         save_best_only=True,
-        verbose=1,
+        verbose=verbose,
     )
     save_best_val_iou = ModelCheckpoint(
-        monitor="val_mean_io_u",
+        monitor="val_iou",
         mode="max",
-        filepath=str(model_cache_path / "val_mean_io_u.ckpt"),
+        filepath=str(model_cache_path / "val_iou.ckpt"),
         save_weights_only=True,
         save_best_only=True,
-        verbose=1,
+        verbose=verbose,
     )
     save_latest = ModelCheckpoint(
         filepath=str(model_cache_path / "latest.ckpt"),
@@ -37,11 +45,43 @@ def get_callbacks(
 
     if early_stopping:
         earlystop_callback = EarlyStopping(
-            monitor='val_loss',
-            min_delta=0.0001,
-            patience=5,
+            monitor='val_iou',
+            min_delta=0.001,
+            patience=20,
             verbose=1,
         )
         callbacks.append(earlystop_callback)
 
     return callbacks
+
+
+class Trainer:
+    def __init__(self, name: str, model: Model, dataset: Dataset) -> None:
+        self.name = name
+        self.model = model
+        self.dataset = dataset
+        self.cache_path = dataset.cache.parent_dir
+
+    def callbacks(self):
+        callbacks = get_callbacks(
+            model_name=self.name,
+            cache_path=self.cache_path,
+            early_stopping=True,
+        )
+        tensorboard_callback = TensorBoard(
+            log_dir=str(self.cache_path / "tensorboard" / self.name),
+            update_freq="epoch",
+        )
+        callbacks.append(tensorboard_callback)
+        return callbacks
+
+    def train(self, verbose: int = 0, **kwargs):
+        self.train_kwargs, self.test_dataset = self.dataset.tf_dataset(
+            **kwargs,
+            rgb=self.model.input.shape[3] in (3, 4),
+        )
+        self.model.fit(
+            **self.train_kwargs,
+            callbacks=self.callbacks(),
+            verbose=verbose,
+        )
