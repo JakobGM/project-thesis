cc45bf8b0ac360b94f7fc1f61a2c0fc10bf4f713
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-12-13 23:59:27 +0100

    plot: Export new functions from plot library

--- remsen/plot/__init__.py
+++ remsen/plot/__init__.py
@@ -1,5 +1,8 @@
 from remsen.plot.data import imshow_with_mask  # noqa: ignore
-from remsen.plot.evaluation import plot_training  # noqa: ignore
+from remsen.plot.evaluation import (  # noqa: ignore
+    plot_training,
+    metric_correlation,
+)
 from remsen.plot.figures import plot_segmentation_types  # noqa: ignore
 from remsen.plot.statistics import (  # noqa: ignore
     plot_bbox_distribution,
