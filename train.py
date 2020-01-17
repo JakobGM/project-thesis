22050d3c6b6466a3234bcc74fa45a011904c3b81
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2020-01-17 09:33:30 +0100

    train: Add new experiments

--- train.py
+++ train.py
@@ -8,11 +8,37 @@ if False:
     model = unet(img_channels=1, loss="iou_loss")
     trainer = Trainer(name="lidar_jaccard_loss", model=model, dataset=dataset)
     trainer.train(epochs=100)
-elif True:
+elif False:
     model = unet(img_channels=1, loss="dice_loss")
     trainer = Trainer(name="lidar_dice_loss", model=model, dataset=dataset)
     trainer.train(epochs=100)
 elif False:
-    model = unet(img_channels=1, loss="iou_loss", dropout=False)
+    model = unet(img_channels=1, dropout=False)
     trainer = Trainer(name="lidar_without_dropout", model=model, dataset=dataset)
     trainer.train(epochs=100)
+elif False:
+    model = unet(img_channels=1)
+    trainer = Trainer(name="lidar_without_augment", model=model, dataset=dataset)
+    trainer.train(epochs=100, augment=False)
+elif False:
+    # Remember to edit input_tile_normalizer
+    model = unet(img_channels=1)
+    trainer = Trainer(name="lidar_metric_normalization", model=model, dataset=dataset)
+    trainer.train(epochs=100)
+elif False:
+    model = unet(img_channels=1, batch_normalization=False)
+    trainer = Trainer(name="lidar_without_batch_normalization", model=model, dataset=dataset)
+    trainer.train(epochs=100)
+elif False:
+    model = unet(img_channels=1)
+    trainer = Trainer(name="lidar_no_area_filter", model=model, dataset=dataset)
+    trainer.train(epochs=89, minimum_building_area=-1)
+elif False:
+    model = unet(img_channels=1)
+    trainer = Trainer(name="lidar_rerun", model=model, dataset=dataset)
+    trainer.train(epochs=89)
+elif True:
+    # TODO: Run this!
+    model = unet(img_channels=1)
+    trainer = Trainer(name="lidar_second_rerun", model=model, dataset=dataset)
+    trainer.train(epochs=89)
